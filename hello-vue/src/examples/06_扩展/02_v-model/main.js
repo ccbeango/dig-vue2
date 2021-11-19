f3ee234cb30d3132a698d52d63160c58d88ea57b
Andy <liuyh940@gmail.com>
2021-11-19 11:48:57 +0800

    阅读事件

---
+++ hello-vue/src/examples/06_扩展/02_v-model/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+
+
+new Vue({
+  el: '#app',
+  template: '<div>'
+  + '<input v-model="message" placeholder="edit me">' +
+  '<p>Message is: {{ message }}</p>' +
+  '</div>',
+  data() {
+    return {
+      message: ''
+    }
+  }
+})
--- hello-vue/src/main.js
+++ hello-vue/src/main.js
@@ -1,5 +1,9 @@
 // 编译
-// import './examples/10_编译/01_编译流程/main'
-// import './examples/10_编译/02_parse2ast/main'
-// import './examples/10_编译/03_optimize/main'
-import './examples/10_编译/04_codegen/main'
+// import './examples/05_编译/01_编译流程/main'
+// import './examples/05_编译/02_parse2ast/main'
+// import './examples/05_编译/03_optimize/main'
+// import './examples/05_编译/04_codegen/main'
+
+// 扩展
+// import './examples/06_扩展/01_event/main'
+import './examples/06_扩展/02_v-model/main'
